5c8006a2fc6cdfe8978abfc5b758a6b8ff868456
EdwinDij <edwin.d899@gmail.com>
2023-07-16 16:03:04 +0200

    choix du langage opérationnel

--- index.js
+++ index.js
@@ -3,29 +3,58 @@
 import chalk from "chalk";
 import inquirer from 'inquirer';
 
-const log = console.log
+const log = console.log;
 
 async function welcome() {
-    log(chalk.green('You want to create a new developpement project!'))
-    const answer = await inquirer.prompt({
-        name: 'speciality',
-        type: 'list',
-        message: 'Choose a spéciality between theses choices: \n',
-        choices: [
-            'Frontend',
-            'Backend'
-        ],
+  log(chalk.green('You want to create a new development project!'));
+
+  const answer = await inquirer.prompt({
+    name: 'specialty',
+    type: 'list',
+    message: 'Choose a specialty between these choices: \n',
+    choices: [
+      'Frontend',
+      'Backend'
+    ],
+  });
+
+  return answer.specialty;
+}
+
+async function langage() {
+  const specialty = await welcome();
+  log(chalk.yellow(`You chose ${specialty}`));
+
+  let answer;
+  if (specialty === 'Frontend') {
+    answer = await inquirer.prompt({
+      name: 'front',
+      type: 'list',
+      message: 'Select a language/framework:\n',
+      choices: [
+        'React',
+        'Vue',
+        'Angular',
+      ],
     });
-    return answer.speciality;
+  } else if (specialty === 'Backend') {
+    answer = await inquirer.prompt({
+      name: 'back',
+      type: 'list',
+      message: 'Select a backend language/framework:\n',
+      choices: [
+        'Express',
+        'Nestjs',
+        'Python',
+        'Django',            
+      ],
+    });
+  }
+
+  log(chalk.yellow(`You chose ${answer.front || answer.back}`));
 }
 
-async function choices() {
-    const speciality = await welcome();
-    log(chalk.yellow(`You choose ${speciality}`));
-
-}
-
-choices().catch(err => {
-    console.error(err);
-    process.exit(1);
-})
+langage().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
