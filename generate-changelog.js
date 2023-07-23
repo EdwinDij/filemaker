f5277f0c46a9492f83156ff9fc3a98314ef68bb7
EdwinDij <edwin.d899@gmail.com>
2023-07-23 18:33:24 +0200

    passage a la v1.0.4 changelog en place

--- generate-changelog.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-exec('npm run generate-changelog', (error, stdout, stderr) => {
-  if (error) {
-    console.error('Erreur lors de la génération du journal des modifications :', error);
-  } else {
-    console.log('Journal des modifications généré avec succès !');
-  }
-});
