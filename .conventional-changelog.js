6db2beb51789171d2994eba24c57abd425a273ed
EdwinDij <edwin.d899@gmail.com>
2023-07-23 17:15:50 +0200

    feat:ajout d'un changelog

---
+++ .conventional-changelog.js
@@ -0,0 +1,12 @@
+module.exports = {
+    types: [
+      { type: 'feat', section: 'Fonctionnalités' },
+      { type: 'fix', section: 'Corrections de bugs' },
+      { type: 'chore', hidden: true },
+      { type: 'docs', hidden: true },
+      { type: 'style', hidden: true },
+      { type: 'refactor', hidden: true },
+      { type: 'perf', hidden: true },
+      { type: 'test', hidden: true },
+    ],
+  }
